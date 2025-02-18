6c39b060b1e5cbda718e81da43b7beae3edd5e11
yunso7201 <58196791+yunso7201@users.noreply.github.com>
2025-02-18 16:13:24 +0900

    JS-Projects

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,34 @@
 'use strict';
+
+// 프로젝트 필터링 관련 로직 처리
+let categories = document.querySelector('.categories');
+let projects = document.querySelectorAll('.project');
+let projectsContainer = document.querySelector('.projects')
+categories.addEventListener('click', (event) => {
+  let filter = event.target.dataset.category;
+  if (filter == null) {
+    return;
+  }
+  handleActiveSelection(event.target);
+  filterProjects(filter);
+})
+
+function handleActiveSelection(target) {
+  let active = document.querySelector('.category--selected');
+  active.classList.remove('category--selected');
+  target.classList.add('category--selected');
+}
+
+function filterProjects(filter) {
+  projects.forEach((project) => {
+    if(filter === 'all' || filter === project.dataset.type) {
+      project.style.display = 'block';
+    } else {
+      project.style.display = 'none';
+    }
+  })
+  projectsContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projectsContainer.classList.remove('anim-out');
+  }, 250)
+}
