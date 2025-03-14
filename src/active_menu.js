bc6cf320c62377f4755733abc9b9c2b18407ea7d
yunso7201 <58196791+yunso7201@users.noreply.github.com>
2025-03-14 16:18:56 +0900

    Update active_menu.js

--- src/active_menu.js
+++ src/active_menu.js
@@ -1,13 +1,20 @@
-'use strict';
-
-const sectionIds = ['#home', '#about', '#skills', '#work', '#testimonial', '#contact'];
+const sectionIds = [
+  "#home",
+  "#about",
+  "#skills",
+  "#work",
+  "#testimonial",
+  "#contact",
+];
 const sections = sectionIds.map((id) => document.querySelector(id));
-const navItems = sectionIds.map((id) => document.querySelector(`[href="${id}"]`));
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[href="${id}"]`)
+);
 const visibleSecitons = sectionIds.map(() => false);
 let activeNavItem = navItems[0];
 
 const options = {
-  rootMargin: '-20% 0px 0px 0px',
+  rootMargin: "-20% 0px 0px 0px",
   treshold: [0, 0.98],
 };
 const observer = new IntersectionObserver(observerCallback, options);
@@ -18,10 +25,15 @@ function observerCallback(entries) {
   entries.forEach((entry) => {
     const index = sectionIds.indexOf(`#${entry.target.id}`);
     visibleSecitons[index] = entry.isIntersecting;
-    selectLastOne = index === sectionIds.length - 1 && entry.isIntersecting && entry.intersectionRatio >= 0.95;
+    selectLastOne =
+      index === sectionIds.length - 1 &&
+      entry.isIntersecting &&
+      entry.intersectionRatio >= 0.95;
   });
-  
-  const navIndex = selectLastOne ? sectionIds.length - 1 : findFirstIntersecting(visibleSecitons);
+
+  const navIndex = selectLastOne
+    ? sectionIds.length - 1
+    : findFirstIntersecting(visibleSecitons);
   selectNavItem(navIndex);
 }
 
@@ -32,8 +44,8 @@ function findFirstIntersecting(intersections) {
 
 function selectNavItem(index) {
   const navItem = navItems[index];
-  if(!navItem) return;
-  activeNavItem.classList.remove('active');
+  if (!navItem) return;
+  activeNavItem.classList.remove("active");
   activeNavItem = navItem;
-  activeNavItem.classList.add('active');
+  activeNavItem.classList.add("active");
 }
